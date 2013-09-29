6d5e226fea4dcc696fa0bdc71e7f3da73c12c7b2
Sorcha Bowler <saoili@gmail.com>
2013-09-30 00:17:43 +0100

    added some basic testing

--- UsefulFunctions.py
+++ UsefulFunctions.py
@@ -5,7 +5,7 @@ def primes (n):
     return noBlanks(sieve(n))  
 
 def sieve (n):
-    """Returns a list, (0,n), where the ith element is i if prime and 0 otherwise"""
+    """Returns a list, [0, ... ,n], where the ith element is i if prime and 0 otherwise"""
     primes = range (0,n+1)
     primes [1] = 0
     for i in range (2,n):
@@ -38,7 +38,8 @@ def noBlanks (li):
     return retVal
 
 def distPrimeFacts (n,lSieve): 
-    """returns a list of the distinct prime factors of positive integer n.
+    """returns a list of the distinct prime factors of positive integer n
+    and a sieve of primes up to that number.
     Take lSieve to be a sieve, adds to it if it's length is less than n.
     """ 
     if type(n) != int or n < 1: 
@@ -56,6 +57,12 @@ def distPrimeFacts (n,lSieve):
             while n % prime == 0: 
                n = n/prime 
     return distPrimeFacts, newSieve    
+
+def distinctPrimeFactors (n):
+   """returns a list of the distinct prime factors of positive integer n"""
+   a, b = distPrimeFacts(n,[])
+   return a
+    
 
 
 def pentN (n):
@@ -79,14 +86,6 @@ def nthPent(n):
 def triN (n):
     """takes integer n, returns the nth triangular number"""
     return n*(n+1)/2
-
-def isTri(n):
-    """doesn't work yet!
-    takes integer n, returns True if it is a triangular number, else False
-    returns False for negative numbers"""
-    if n < 0:
-        return False
-    return False
 
 
 def hexN(n):
@@ -157,6 +156,61 @@ def isPandigital(n):
 if __name__ == "__main__":
     "Should put some testing stuff in here for useful functions, haven't yet"""
     print "This is a module of useful functions for Project Euler problems"
+    print "Some basic testing below, will replace with unittest framework later"    
+    
+    """test primes (n): 
+    Returns a list of primes up to and including n (if prime)"""
+    isPrimeWorking = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97] == primes(100)
+    print "primes function works for 100: " + str (isPrimeWorking)
+     
+    """test sieve (n):
+    Returns a list, [0, ... ,n], where the ith element is i if prime and 0 otherwise"""
+    isSieveWorking = [0,0,2,3,0,5,0,7,0,0,0] == sieve(10)
+    print "sieve function works for 10: " + str (isSieveWorking)
+    
+    """test addToSieve(n, sieve): 
+    Takes a sieved list of primes (with spaces still in) and an integer n. 
+    Returns a sieve up to n"""
+    isAddToSieveWorking = [0,0,2,3,0,5,0,7,0,0,0,11,0,13,0,0,0,17,0,19,0] == addToSieve(20, sieve(10))
+    print "addToSieve function works for 20 and sieve up to 10: " + str (isAddToSieveWorking)
+    
+    """test noBlanks (li):
+    takes a list and returns all non zero elements of that list, as a list"""
+    isNoBlanksWorking = [1,2,3,5] == noBlanks([0,1,2,3,0,0,5,0])
+    print "noBlanks function works for [0,1,2,3,0,0,5,0]: " + str(isNoBlanksWorking)
+    
+    """test distinctPrimeFactors (n,lSieve): 
+    returns a list of the distinct prime factors of positive integer n
+    """ 
+    print "distinctPrimeFactors function works for 70: " + str(distinctPrimeFactors(70) == [2,5,7])
+    
+    """test pentN (n):
+    takes integer n, returns the nth pentagonal number"""
+    
+    """test isPent(n):
+    takes integer n, returns True if it is a pentagonal number, else False
+    returns False for negative numbers"""
 
+    """test nthPent(n):
+    takes integer n, returns what number pentagonal number it is if it is one and 0 otherwise"""
 
+    """test triN (n):
+    takes integer n, returns the nth triangular number"""
 
+    """test hexN(n):
+    takes integer n, returns the nth hexagonal number"""
+
+    """test nthDigit (x,n):
+    (x,n) returns the nth digit of x, returns -1 if that digit doesn't exist"""
+
+    """test numDigits (n):
+    returns the number of digits in a positive integer with up to 9 digits"""
+
+    """test isPrime(n):
+    returns True if prime, False if not. For large selections of numbers, use sieve instead"""
+
+    """test reverse(n):
+    returns the reverse of a positive integer"""
+
+    """test isPandigital(n):
+    returns true if n is a 1 to 9 pandigital"""
